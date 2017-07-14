36f7debe68d2fb7a6cd9e814c93cde99f0f4b93b
Ronnie Pinkerton <pinkrdp@gmail.com>
2017-07-14 10:17:20 -0500

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -29,7 +29,7 @@ class ansible::params {
       'python-pkg-resources','python-pip','python-crypto','python-markupsafe',
       'python-httplib2']
     }
-    'Redhat': {
+    'RedHat': {
       $pip_dep_package = ['PyYAML','libyaml','python-babel','python-crypto',
       'python-ecdsa','python-httplib2','python-jinja2','python-keyczar',
       'python-markupsafe','python-paramiko','python-pyasn1','python-six',
